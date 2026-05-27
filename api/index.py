e2e8175e9cba03a03d6828ca483280011af657e5
GooDBoy290 <147719506+GooDBoy290@users.noreply.github.com>
2026-05-27 14:38:30 -0700

    Change CORS policy to allow all origins
    Updated CORS settings to allow all origins.

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://terabox-videodownloader.netlify.app"], # 🔒 Lock it here!
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
